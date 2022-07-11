7d71d676ef6672774c8c03b739894a4ca143fc7b
Joana <joanajanefdez@gmail.com>
2022-07-11 13:46:14 +0200

    EJ 1 completado,valorar complejidad excesiva

--- src/U7_EJ1App.java
+++ src/U7_EJ1App.java
@@ -11,47 +11,65 @@ public class U7_EJ1App {
 		 * guardarla en un hashtable, alumno:notaMedia.
 		 * datos proporcionados por user
 		 */
+						
+		Scanner lector = new Scanner(System.in);
 		
-		Scanner sc = new Scanner(System.in);
+		Hashtable<String,Integer> avgGradeStudent = new Hashtable<String,Integer>(); //inicializamos hastable que vamos a usar
 		
-		String student;
-				
-		ArrayList<Integer> grades = new ArrayList<Integer>();
-		Hashtable<String,Integer> avgGradeStudent = new Hashtable<String,Integer>();
-				
-		int sum = 0;
-		int avg;
+		System.out.println("Cuantos alumnos vas a introducir?");
+		int students = lector.nextInt();
+		lector.nextLine();
 		
-		System.out.println("Indica nombre del alumno");
-		student = sc.nextLine();
+		for(int i=0;i<students;i++) {
+			addStudent(avgGradeStudent,lector); //introduciremos tantos como hemos indicado por teclado
+		}
 		
-		System.out.println("Indica las notas del alumno del curso. Introduce -1 cuando hayas terminado");	
-		boolean end = false;
+		System.out.println(avgGradeStudent.toString()); //imprimimos key y valor de la hastable 
+		
+		lector.close(); //cerramos el scanner que hemos estado usando
+		
+	}
 
+	private static void addStudent(Hashtable<String,Integer> avgGradeStudent,Scanner lector) {
+			
+		System.out.println("Indica nombre del alumno");
+		String student = lector.nextLine();
+		
+		System.out.println("Indica las notas del alumno del curso. Introduce -1 cuando hayas terminado");		
+		ArrayList<Integer> grades = addGrades(lector);		//en grades guardamos la lista de notas
+				
+		avgGradeStudent.put(student,resultAvgGrade(grades));	//añadimos al hastable, el nombre y la nota media calculada
+		
+	}
+
+	private static ArrayList<Integer> addGrades(Scanner lector) {
+		ArrayList<Integer> grades = new ArrayList<Integer>(); //creamos el arrayList como auxiliar para guardar las notas que sean
+																//se reinicia en cada llamada del metodo		
+		boolean end = false;
 		while(!end){
-			if(sc.hasNextInt()) {
-				int nota = sc.nextInt();			
+			if(lector.hasNextInt()) {
+				int nota = lector.nextInt();			
 				if(nota!=-1) {
 					grades.add(nota);
 				}else {
 					end = true;
-					sc.nextLine();
+					lector.nextLine();
 				}				
 			}			
 		}
-		
-		sc.close();
+		return grades;
+	}
+
+	private static int resultAvgGrade(ArrayList<Integer> grades) {  //calculo nota media 
+		int sum = 0;
+		int avg;
 		
 		Iterator<Integer> it = grades.iterator();
 		while(it.hasNext()) {
 			sum += it.next();
 		}
 		avg = sum/grades.size();
-		
-		System.out.println(avg);
-		
-		avgGradeStudent.put(student,avg);
-		
+		return avg;
 	}
 
 }
